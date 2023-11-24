77c2bc24b9280324201fc8924094e24ed606994d
PhamDuyHau <phamduyhau2000@gmail.com>
2023-11-24 11:45:21 +0700

    Hau da xong lan ba roi lan 2

--- resources/views/frontend/aboutus/aboutus.blade.php
+++ resources/views/frontend/aboutus/aboutus.blade.php
@@ -2,21 +2,22 @@
 @section('title', 'aboutus')
 @section('main')
 
+<link rel="stylesheet" href="{{asset('aboutuscss/aboutus.css')}}">
 
-
+<br>
+<br>
 {{-- Mission sector --}}
 <div class="container mt-3 about-main" data-aos="zoom-in-down">
-    <br>
-    <br>
-    <br>
     <h1>What is our mission</h1>
-    <span>
+
+    <span class="mission">
         At StayHopeful, we are dedicated to creating positive and lasting change in the world.
         Our mission is to extend a helping hand to those facing the challenges of disease, combat animal cruelty, and provide relief in the aftermath of natural disasters.
         We believe in the power of collective compassion to heal, protect, and rebuild lives.
         Through strategic initiatives and community-driven support, we strive to make a meaningful impact, fostering a world where every individual, animal, and community can thrive despite adversity.
         Join us in our mission to bring hope, healing, and resilience to those in need.
     </span>
+
     <img src="{{ asset('img/aboutus_main.jpg') }}" alt="aboutus_main" class="aboutus-image-main">
 </div>
 
@@ -30,6 +31,7 @@
         <div class="col story1">
             
             <div>
+                
                 <span> As a small group of passionate individuals, we founded <strong>Stay Hope Full</strong> with a shared commitment to making a difference. Inspired by the stories of resilience in the face of adversity, we set out on a mission to combat diseases, end animal cruelty, and respond to natural disasters. Over the years, we've faced challenges head-on, but the stories of transformation keep us driven.</span>
             </div>
             
@@ -56,6 +58,7 @@
             <div>
                 <div>
                   <span>Meet <strong>Sarah, a survivor of heart disease</strong>. Through the generosity of donors like you, we were able to provide life-saving treatment and support her on the journey to recovery. Sarah's story is just one example of the impact we can make together.</span>
+                    
                 </div>
             </div>
         </div>
@@ -69,6 +72,7 @@
             <div>
                 
                 <div>
+                    
                     <span>From humble beginnings, our organization has grown, forming partnerships with local communities and international organizations. Together, we've responded to natural disasters, rescued animals from abusive situations, and brought hope to countless lives.</span>
                 </div>
             </div>
@@ -106,6 +110,7 @@
         <div class="col story5">
             <div>
                 <div>
+                    
                     <span>Join us on this incredible journey. Your support can be the turning point in someone's life, and together, we can create a world where every individual, human or animal, has the opportunity to thrive</span>
                 </div>
             </div>
@@ -119,6 +124,91 @@
     </div>
 </div>
 
+{{-- Accordion sector--}}
+<div class="container mt-3">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+
+        <!-- Image -->
+        <div data-aos="zoom-in-right">
+            <img class="custom-badge" src="{{ asset('img/badge.jpg') }}" alt="badge">
+        </div>
+
+        {{-- Accordion --}}
+        <div class="accordion" id="projectAccordion" data-aos="zoom-in-left">
+            <!-- Projects Funded -->
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="projectFundedHeading">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#projectFundedCollapse" aria-expanded="true" aria-controls="projectFundedCollapse">
+                        <i class="bi bi-check-square"></i> Projects Funded
+                    </button>
+                </h2>
+                <div id="projectFundedCollapse" class="accordion-collapse collapse show" aria-labelledby="projectFundedHeading" data-bs-parent="#projectAccordion">
+                    <div class="accordion-body">
+                        We have successfully funded <strong>50</strong> impactful projects, creating positive change globally. These projects include initiatives related to clean water, education, healthcare, and environmental sustainability.
+                    </div>
+                </div>
+            </div>
+        
+            <!-- People Served -->
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="peopleServedHeading">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#peopleServedCollapse" aria-expanded="false" aria-controls="peopleServedCollapse">
+                        <i class="bi bi-person"></i> People Served
+                    </button>
+                </h2>
+                <div id="peopleServedCollapse" class="accordion-collapse collapse" aria-labelledby="peopleServedHeading" data-bs-parent="#projectAccordion">
+                    <div class="accordion-body">
+                        Our projects have positively impacted the lives of <strong>100,000</strong> individuals around the world. We focus on reaching underserved communities, providing them with access to essential services and improving their overall well-being.
+                    </div>
+                </div>
+            </div>
+        
+            <!-- Countries Impacted -->
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="countriesImpactedHeading">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#countriesImpactedCollapse" aria-expanded="false" aria-controls="countriesImpactedCollapse">
+                        <i class="bi bi-globe"></i> Countries Impacted
+                    </button>
+                </h2>
+                <div id="countriesImpactedCollapse" class="accordion-collapse collapse" aria-labelledby="countriesImpactedHeading" data-bs-parent="#projectAccordion">
+                    <div class="accordion-body">
+                        Our projects have made a positive impact in <strong>25</strong> countries, reaching communities far and wide. We collaborate with local partners and organizations to address specific challenges and contribute to sustainable development.
+                    </div>
+                </div>
+            </div>
+        
+            <!-- Challenges Faced -->
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="challengesFacedHeading">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#challengesFacedCollapse" aria-expanded="false" aria-controls="challengesFacedCollapse">
+                        <i class="bi bi-exclamation-circle"></i> Challenges Faced
+                    </button>
+                </h2>
+                <div id="challengesFacedCollapse" class="accordion-collapse collapse" aria-labelledby="challengesFacedHeading" data-bs-parent="#projectAccordion">
+                    <div class="accordion-body">
+                        Despite our successes, we have faced various challenges along the way. These challenges have strengthened our resolve and commitment to making a lasting impact on the lives of those in need.
+                    </div>
+                </div>
+            </div>
+        
+            <!-- Future Plans -->
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="futurePlansHeading">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#futurePlansCollapse" aria-expanded="false" aria-controls="futurePlansCollapse">
+                        <i class="bi bi-arrow-right-circle"></i> Future Plans
+                    </button>
+                </h2>
+                <div id="futurePlansCollapse" class="accordion-collapse collapse" aria-labelledby="futurePlansHeading" data-bs-parent="#projectAccordion">
+                    <div class="accordion-body">
+                        Looking ahead, our future plans involve expanding our reach, launching new projects, and leveraging innovative solutions to address emerging challenges. We are dedicated to creating a positive and sustainable impact on a global scale.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 {{-- call to action sector --}}
 <div class="container mt-3 call_to_action">
     <h3>Join Us in Making a Difference!</h3>
@@ -129,6 +219,7 @@
 
         <div class="col" data-aos="fade-left" data-aos-duration="1000">
             <div class="card">
+                <br>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg"  width="100" height="100" viewBox="0 0 512 512">
                         <style>svg{fill:#15b5f9}</style><path d="M256 48a160 160 0 1 1 0 320 160 160 0 1 1 0-320zm0 368A208 208 0 1 0 256 0a208 208 0 1 0 0 416zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.6-64-64-64c-13.6 18.2-29.8 34.3-48 48h48c8.8 0 16 7.2 16 16v32c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V416c0-8.8 7.2-16 16-16h48c-18.2-13.7-34.3-29.8-48-48zM276 104c0-11-9-20-20-20s-20 9-20 20v14c-7.6 1.7-15.2 4.4-22.2 8.5c-13.9 8.3-25.9 22.8-25.8 43.9c.1 20.3 12 33.1 24.7 40.7c11 6.6 24.7 10.8 35.6 14l1.7 .5c12.6 3.8 21.8 6.8 28 10.7c5.1 3.2 5.8 5.4 5.9 8.2c.1 5-1.8 8-5.9 10.5c-5 3.1-12.9 5-21.4 4.7c-11.1-.4-21.5-3.9-35.1-8.5c-2.3-.8-4.7-1.6-7.2-2.4c-10.5-3.5-21.8 2.2-25.3 12.6s2.2 21.8 12.6 25.3c1.9 .6 4 1.3 6.1 2.1l0 0 0 0c8.3 2.9 17.9 6.2 28.2 8.4V312c0 11 9 20 20 20s20-9 20-20V298.2c8-1.7 16-4.5 23.2-9c14.3-8.9 25.1-24.1 24.8-45c-.3-20.3-11.7-33.4-24.6-41.6c-11.5-7.2-25.9-11.6-37.1-15l-.7-.2c-12.8-3.9-21.9-6.7-28.3-10.5c-5.2-3.1-5.3-4.9-5.3-6.7c0-3.7 1.4-6.5 6.2-9.3c5.4-3.2 13.6-5.1 21.5-5c9.6 .1 20.2 2.2 31.2 5.2c10.7 2.8 21.6-3.5 24.5-14.2s-3.5-21.6-14.2-24.5c-6.5-1.7-13.7-3.4-21.1-4.7V104z"/>
@@ -144,7 +235,8 @@
 
         <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
 
-            <div class="card" ata-aos="fade-up" data-aos-duration="3000">
+            <div class="card">
+                <br>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 640 512">
                         <style>svg{fill:#15b5f9}</style><path d="M396.6 6.5L235.8 129.1c9.6 1.8 18.9 5.8 27 12l168 128c13.2 10.1 22 24.9 24.5 41.4l6.2 41.5H608c9.3 0 18.2-4.1 24.2-11.1s8.8-16.4 7.4-25.6l-24-160c-1.2-8.2-5.6-15.7-12.3-20.7l-168-128c-11.5-8.7-27.3-8.7-38.8 0zm-153.2 160c-11.5-8.7-27.3-8.7-38.8 0l-168 128c-6.6 5-11 12.5-12.3 20.7l-24 160c-1.4 9.2 1.3 18.6 7.4 25.6S22.7 512 32 512H224V352l96 160h96c9.3 0 18.2-4.1 24.2-11.1s8.8-16.4 7.4-25.6l-24-160c-1.2-8.2-5.6-15.7-12.3-20.7l-168-128z"/>
@@ -160,9 +252,10 @@
             </div>
         </div>
 
-        <div class="col" class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="400">
+        <div class="col" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="400">
             
             <div class="card">
+                <br>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 640 512">
                         <style>svg{fill:#15b5f9}</style><path d="M544 248v3.3l69.7-69.7c21.9-21.9 21.9-57.3 0-79.2L535.6 24.4c-21.9-21.9-57.3-21.9-79.2 0L416.3 64.5c-2.7-.3-5.5-.5-8.3-.5H296c-37.1 0-67.6 28-71.6 64H224V248c0 22.1 17.9 40 40 40s40-17.9 40-40V176c0 0 0-.1 0-.1V160l16 0 136 0c0 0 0 0 .1 0H464c44.2 0 80 35.8 80 80v8zM336 192v56c0 39.8-32.2 72-72 72s-72-32.2-72-72V129.4c-35.9 6.2-65.8 32.3-76 68.2L99.5 255.2 26.3 328.4c-21.9 21.9-21.9 57.3 0 79.2l78.1 78.1c21.9 21.9 57.3 21.9 79.2 0l37.7-37.7c.9 0 1.8 .1 2.7 .1H384c26.5 0 48-21.5 48-48c0-5.6-1-11-2.7-16H432c26.5 0 48-21.5 48-48c0-12.8-5-24.4-13.2-33c25.7-5 45.1-27.6 45.2-54.8v-.4c-.1-30.8-25.1-55.8-56-55.8c0 0 0 0 0 0l-120 0z"/>
@@ -180,9 +273,9 @@
 </div>
 
 {{-- Our Founder sector --}}
-<div class="container mt-3 our_founder">
+<div class="container mt-3 our_founder" data-aos="zoom-in">
     <h2 class="text-center">Our Founder</h2>
-    <div class="team-description">
+    <div>
         Welcome to the heart of <strong>Stay Hope full</strong>. Behind every impactful initiative and transformative project, there's a team of dedicated individuals committed to making a positive difference. Together, we share a common vision, diverse skills, and an unwavering commitment to creating a world where hope, compassion, and resilience thrive.
     </div>
 
@@ -193,33 +286,124 @@
             <div class="col-md-4 text-center">
                 <img class="img-fluid rounded-circle founder-img" src="{{ asset('img/aboutus_founder.jpg') }}" alt="founder">
             </div>
-
+            
             <!-- Introduction -->
             <div class="col-md-8">
+                <br>
+            <br>
+            <br>
                 <p class="text-justify">
                     Meet John Doe, the visionary behind <strong>Stay hope full</strong>. As the driving force behind the organization's mission, John's passion for making a positive impact has been evident since its inception. His leadership and commitment inspire our team to create a world where hope and compassion thrive. Learn more about John's journey and dedication in the Read More section below.
                 </p>
-                <a href="#founder-story" class="btn btn-primary">HEAR JOHN DOE's STORY</a>
+                <a href="{{route("aboutus.johndoe")}}" class="btn btn-primary">HEAR JOHN DOE's STORY</a>
             </div>
         </div>
     </div>
 </div>
+<hr>
+<!-- Team Members sector -->
+<div class="container mt-3">
 
+    <div data-aos="zoom-out-up">
+        <h2 class="text-center">Our Team</h2>
+        <span>
+            Welcome to the heart of <strong>Stay Hopeful</strong>. Our dedicated team shares a common vision, diverse skills, and an unwavering commitment to creating a world where hope, compassion, and resilience thrive.
+        </span>
+    </div>
 
-<!-- Board of Directors section -->
-<div class="container mt-5 text-center board_of_directors">
-    <h2>Board of Directors</h2>
+    <br>
+    <div class="row row-cols-1 row-cols-md-2 g-4" >
+      <div class="col">
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-4" data-aos="fade-up-right" data-aos-delay="200">
+              <img src="{{ asset('img/aboutus2.jpg') }}" class="img-fluid rounded-start" alt="">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" data-aos="fade-up-right" data-aos-delay="200">
+                <h5 class="card-title">Jane Smith</h5>
+                <p class="card-text">Jane Smith is our Chief Marketing Officer, bringing a creative and strategic approach to our outreach efforts. Her dedication has played a crucial role in expanding our reach and impact.</p>
+                <p class="card-text"><small class="text-body-secondary">Date of Birth: March 10, 1988</small></p>
+                <a href="{{route("aboutus.janesmith")}}" class="btn btn-primary">View Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  
+    <div class="col">
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-4" data-aos="fade-up-left" data-aos-delay="400">
+              <img src="{{ asset('img/aboutus2.jpg') }}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" data-aos="fade-up-left" data-aos-delay="400">
+                <h5 class="card-title">Robert Johnson</h5>
+                <p class="card-text">Robert Johnson, our Chief Financial Officer, brings extensive experience in finance and strategic planning. His innovative thinking has significantly contributed to our organization's success.</p>
+                <p class="card-text"><small class="text-body-secondary">Date of Birth: August 22, 1982</small></p>
+                <a href="{{route("aboutus.robertjohnson")}}" class="btn btn-primary">View Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  
+    <div class="col">
+        <div class="card">
+          <div class="row g-0">
+            <div class="col-md-4" data-aos="fade-down-right" data-aos-delay="500">
+              <img src="{{ asset('img/aboutus2.jpg') }}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body" data-aos="fade-down-right" data-aos-delay="500">
+                <h5 class="card-title">Kai Greene</h5>
+                <p class="card-text">Kai Greene, our Lead Developer, playing a key role in shaping our technological initiatives. Their expertise has been instrumental in driving our digital transformation.</p>
+                <p class="card-text"><small class="text-body-secondary">Date of Birth: May 18, 1989</small></p>
+                <a href="{{route("aboutus.kaigreene")}}" class="btn btn-primary">View Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
 
-    <div class="team-container">
-        <div class="team-names" id="teamNames">
-            <div class="team-member" onclick="showIntroduction(1)">Team Member 1</div>
-            <div class="team-member" onclick="showIntroduction(2)">Team Member 2</div>
-            <div class="team-member" onclick="showIntroduction(3)">Team Member 3</div>
-            <!-- Add more team members as needed -->
+    <div class="col">
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4" data-aos="fade-down-left" data-aos-delay="700">
+                    <img src="{{ asset('img/aboutus2.jpg') }}" class="img-fluid rounded-start" alt="Person's Image">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body" data-aos="fade-down-left" data-aos-delay="700">
+                        <h5 class="card-title">Oliver Hudson</h5>
+                        <p class="card-text">liver Hudson serves as our Creative Director, bringing a unique blend of artistic vision and strategic thinking to our projects. Their creativity adds a distinctive touch to our initiatives.</p>
+                        <p class="card-text"><small class="text-body-secondary">Date of Birth: January 15, 1985</small></p>
+                        <a href="{{route("aboutus.oliverhudson")}}" class="btn btn-primary">View Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+{{-- volunteer sector --}}
+<div class="container volunteer" >
+    <div class="row row-cols-1 row-cols-md-2 g-10 volunteer" data-aos="zoom-in-right">
+        <div class="col left-volunteer">
+            <div>
+                <img src="{{ asset('img/aboutus6.jpg') }}" alt="aboutus4" class="aboutus-image6">
+            </div>
+            
         </div>
 
-        <div class="team-introduction" id="teamIntroduction">
-            <!-- Introduction content will be dynamically loaded here -->
+        <div class="col right-volunteer" data-aos="zoom-in-left">
+            <div>
+                <div>
+                    <h3>Change the world with us</h3>
+                    <span>We are hiring! Explore our openings and join the team</span>
+                    <a href="#">Careers</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
---
+++ resources/views/frontend/aboutus/johndoe.blade.php
@@ -0,0 +1,128 @@
+@extends('frontend.site')
+@section('title', 'johndoe')
+@section('main')
+
+
+<link rel="stylesheet" href="{{asset('aboutuscss/johndoe.css')}}">
+
+
+<br>
+<br>
+<h1>Meet John Doe, The Founder</h1>  
+        <br>
+        <div class="container mt-3 introduction">
+            {{-- introduction_1 --}}
+            
+            <div class="row">
+                <a href="{{ route('aboutus.index') }}" class="about-us-link" style="display: inline-block; margin-bottom: 10px;">
+                    <i class="fa fa-arrow-left" style="margin-right: 5px;"></i> Return to About Us page
+                </a>
+                <div class="col-md-10 offset-md-1">
+                    
+                    <span class="introduction_1">
+                        John Doe, a compassionate soul from a modest background, grew up witnessing the impact of community
+                        support. Raised in a town where neighbors looked out for one another, he developed a deep sense of
+                        empathy and a belief in the power of collective goodwill.
+                    </span>
+                    <br>
+                    <span>
+                        After earning a degree in business administration, John worked in various nonprofit organizations,
+                        where he witnessed the challenges they faced in fundraising and connecting with donors. Inspired by
+                        the potential to leverage technology for social good, John decided to combine his business acumen
+                        with his passion for making a difference.
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container mt-3 the_past">
+            <h5>Challenges and Triumphs:</h5>
+            <div class="clearfix">
+                <img src="{{ asset('img/johndoe_past.jpg') }}" class="col-md-6 float-md-end mb-3 ms-md-3 johndoe_image"
+                    alt="johndoe_past">
+
+                <span>
+                    Founding a donation-based website wasn't without hurdles. In the early stages, John faced skepticism
+                    about the feasibility of a digital platform for charitable giving. Funding was scarce, and convincing
+                    potential donors of the platform's legitimacy posed a significant challenge.
+                </span>
+                <br>
+                <span>
+                    Undeterred, John poured his energy into building a transparent and user-friendly website. He
+                    collaborated with nonprofits to ensure their needs were met and donors could see the direct impact of
+                    their contributions. Overcoming initial resistance, the platform gained traction, and John's
+                    commitment to transparency and efficiency started to pay off.
+                </span>
+                <br>
+                <span>
+                    Triumph came when the website facilitated a major fundraising campaign that surpassed all
+                    expectations. Donors appreciated the platform's simplicity and the tangible outcomes of their
+                    contributions. John's commitment to bridging the gap between those in need and those willing to help
+                    had become a reality.
+                </span>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container mt-3 becoming_the_founder_main">
+            <h5>Current Projects and Future Plans:</h5>
+            <div class="becoming_the_founder2">
+
+
+                <span class="col-md-10 offset-md-1">
+                    With the success of his donation website, John is now working on expanding its reach and impact. He's
+                    developing partnerships with international organizations to address global challenges, from education
+                    disparities to environmental conservation.
+                </span>
+
+                <span class="col-md-10 offset-md-1">
+                    John is also exploring innovative ways to use technology, such as blockchain, to enhance transparency
+                    in donation tracking. He envisions a future where donors can trace the journey of their contribution
+                    from the moment it's made to the tangible impact it creates.
+                </span>
+
+                <span class="col-md-10 offset-md-1">
+                    Looking ahead, John is passionate about fostering a culture of giving. He's initiating educational
+                    programs to teach young people about the joy of philanthropy and the importance of making a positive
+                    impact on the world. John dreams of a future where the act of giving is woven into the fabric of
+                    society, creating a more compassionate and interconnected world.
+                </span>
+                <p class="col-md-10 offset-md-1">
+                    He Quote: "I believe in the power of giving, not as a one-time act but as a lifelong commitment to building
+                    bridges between those who have and those who need. Together, we can turn the page on a new chapter of
+                    empathy and change."
+                </p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container mt-3 the_present">
+
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <br>
+                    <span>
+                        Watch how a decision to reset his life led John Doe to build charity.
+                    </span>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="video-container">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/your_video_id" frameborder="0"
+                            allowfullscreen></iframe>
+                    </div>
+                </div>
+                <a href="{{ route('aboutus.index') }}" class="about-us-link" style="display: inline-block; margin-top: 10px;">
+                    <i class="fa fa-arrow-left" style="margin-right: 5px;"></i> Return to About Us page
+                </a>
+            </div>
+        </div>
+
+
+@endsection
