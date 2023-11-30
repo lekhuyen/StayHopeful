0f18c196e747d848d0176aff234cdd2c41786fb8
Pearlicia <suonghbts2302004@fpt.edu.vn>
2023-11-30 12:02:52 +0700

    chinh sua trang Sensitive

--- resources/views/sensitive/create.blade.php
+++ resources/views/sensitive/create.blade.php
@@ -1,5 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="{{ asset('feedbackcss/create__sensitive.css') }}">
+
 <a href="{{ route('sensitive.index') }}" class="btn btn-light"></a>
 @if (session('success'))
     <div class="alert alert-success">
@@ -8,28 +10,33 @@
 @endif
 <form action="{{ route('sensitive.store') }}" method="post">
     @csrf
-    <div class="mb-3 mt-3">
-        <label for="word">Word:</label>
-        <input type="text" class="form-control" id="word" value="{{ old('word') }}" placeholder="Enter word"
-            name="word">
-        @error('word')
-            <span class="text-danger">{{ $message }}</span>
-        @enderror
+    <div class="sensitive__container">
+        <h1>Add Sensitive Word Form</h1>
+        <div class="mb-3 mt-3">
+            <label for="word">Word:</label>
+            <input type="text" class="form-control" id="word" value="{{ old('word') }}"
+                placeholder="Enter a sensitive word" name="word">
+            @error('word')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <div class="form-check radio__sensitive">
+                <input type="radio" class="form-check-input" id="radio1"
+                    {{ old('status') == '1' ? 'checked' : '' }} name="status" value="1" checked>
+                <label class="form-check-label" for="radio1">Active</label>
+            </div>
+            <div class="form-check radio__sensitive">
+                <input type="radio" class="form-check-input" {{ old('status') == '0' ? 'checked' : '' }}
+                    id="radio2" name="status" value="0">
+                <label class="form-check-label" for="radio2">Deactive</label>
+            </div>
+        </div>
+        <div class="btn__sensitive">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
     </div>
 
-    <div class="mb-3">
-        <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio1" {{ old('status') == '1' ? 'checked' : '' }}
-                name="status" value="1" checked>
-            <label class="form-check-label" for="radio1">Active</label>
-        </div>
-        <div class="form-check">
-            <input type="radio" class="form-check-input" {{ old('status') == '0' ? 'checked' : '' }} id="radio2"
-                name="status" value="0">
-            <label class="form-check-label" for="radio2">Deactive</label>
-        </div>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
