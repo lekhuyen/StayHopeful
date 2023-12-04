6912b44bb76fa6809eda2c26e5500f794ed32dcc
IYC054 <trieunhananh@gmail.com>
2023-12-04 20:27:44 +0700

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -235,18 +235,18 @@
 
     @include('frontend/login/login');
     @include('frontend/profile/popup_profile');
-
+    @endif
 
     {{-- đừng xoá --}}
     {{-- @foreach ($totalAmountByProject as $projectId => $total)
         Project ID: {{ $projectId }}, Total Amount: {{ $total }}<br>
     @endforeach --}}
-    @if (session('isVerified'))
+    {{-- @if (session('isVerified'))
         @include('frontend/login/login', ['isVerified', true]);
     @else
         @include('frontend/login/login');
 
-    @endif
+    @endif --}}
     <script src="https://cdn.jsdelivr.net/npm/keen-slider@6.8.6/keen-slider.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/odometer.js/0.4.7/odometer.min.js"
         integrity="sha512-v3fZyWIk7kh9yGNQZf1SnSjIxjAKsYbg6UQ+B+QxAZqJQLrN3jMjrdNwcxV6tis6S0s1xyVDZrDz9UoRLfRpWw=="
