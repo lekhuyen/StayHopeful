ac7e21f97999610047ba2514d022cebfc5bfd228
PhamDuyHau <phamduyhau2000@gmail.com>
2023-12-11 21:20:20 +0700

    Sua title Again

--- resources/views/frontend/adminpage/manager/design.blade.php
+++ resources/views/frontend/adminpage/manager/design.blade.php
@@ -127,7 +127,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Thêm Slider</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Add Slider</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form method="POST" enctype="multipart/form-data" action="{{ route('admin.create_slider') }}">
--- resources/views/frontend/adminpage/manager/maildetail.blade.php
+++ resources/views/frontend/adminpage/manager/maildetail.blade.php
@@ -1,5 +1,6 @@
 @extends('frontend.adminpage.index')
 @section('admin_content')
+<link rel="stylesheet" href="{{ asset('feedbackcss/sensitive.css') }}">
 <h1>Detail Mail</h1>
     {!! $mail->message !!}
 @endsection
