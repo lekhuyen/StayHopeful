6fe8e65edbac9412ec1751ec33a7ee2e58fd09e3
IYC054 <trieunhananh@gmail.com>
2023-12-13 20:35:24 +0700

    add table otp

--- app/Models/Otp.php
+++ app/Models/Otp.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Otp extends Model
 {
+    protected $fillable = ['user_email', 'otp', 'otp_attempts'];
+    protected function user(){
+        return $this->belongsTo(User::class, 'user_email', 'email');
+    }
     use HasFactory;
 }
