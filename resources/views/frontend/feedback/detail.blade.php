4cd2c24da9b1a323891be126758cc052c4967f60
Pearlicia <suonghbts2302004@fpt.edu.vn>
2023-12-14 00:42:54 +0700

    hoan thanh chinh sua footer cho Dashboard a~

--- resources/views/frontend/feedback/detail.blade.php
+++ resources/views/frontend/feedback/detail.blade.php
@@ -1,8 +1,8 @@
 @extends('frontend.adminpage.index')
 @section('admin_content')
     {{-- css --}}
-    <link rel="stylesheet" href="{{ asset('feedbackcss/sensitive.css') }}">
     <link rel="stylesheet" href="{{ asset('general/general.css') }}">
+    <link rel="stylesheet" href="{{ asset('feedbackcss/sensitive.css') }}">
     {{-- css --}}
 
     <div class="btn__back">
@@ -23,10 +23,10 @@
             <tbody>
                 <tr>
                     <td>{{ $feedback->email }}</td>
-                    <td>{{ $feedback->content }}</td>
+                    <td><span class="content__feedback__detail">{{ $feedback->content }}</span></td>
                     <td>{{ $feedback->star }}</td>
                     <td>
-                        <button class="btn btn-danger d-none btn-bad-feedback">Bad Feedback</button>
+                        <button class="btn btn-danger btn-bad-feedback btn-sm d-none disabled">Bad Feedback</button>
                     </td>
                 </tr>
             </tbody>
@@ -34,11 +34,13 @@
     </div>
     <script>
         const wordSensitives = @json($words);
-        const row = document.querySelector('.content');
+        const row = document.querySelector('.content__feedback__detail');
+        console.log("bad: ", row);
         const btnBadFeedback = document.querySelector('.btn-bad-feedback');
         let text = row.textContent.toLowerCase();
         wordSensitives.forEach(word => {
             if (text.includes(word)) {
+
                 text = text.replace(word, `<b style="color:red">${word}</b>`);
                 btnBadFeedback.classList.remove('d-none');
             }
