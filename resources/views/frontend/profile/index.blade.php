a737e9df27dd3ea31584c952b40b0911f9a4168f
Pearlicia <suonghbts2302004@fpt.edu.vn>
2023-12-19 15:41:15 +0700

    dang chinh sua mail template

--- resources/views/frontend/profile/index.blade.php
+++ resources/views/frontend/profile/index.blade.php
@@ -268,7 +268,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-success">Save</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                     </form>
                 </div>
             </div>
