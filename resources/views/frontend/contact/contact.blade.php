6a89e6b55a2a119f2c40e7d971548fa1f57a4699
PhamDuyHau <phamduyhau2000@gmail.com>
2023-12-20 20:30:16 +0700

    about us 7:00 20/12

--- resources/views/frontend/contact/contact.blade.php
+++ resources/views/frontend/contact/contact.blade.php
@@ -79,9 +79,7 @@
                     style="text-decoration: none; color: white;">contact@StayHopeful.org</a></p>
             <p>Head Office Address: </p>
             <div class="map-container" style="padding-bottom: 20px">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d823.8609185302755!2d106.70858875101122!3d10.813985461549624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1700053839649!5m2!1sen!2s"
-                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d692.8415149638578!2d106.70265068221642!3d10.78805334576912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f4b3b357777%3A0x292e9b68d9d46aa6!2zTmd1eeG7hW4gQuG7iW5oIEtoacOqbS8yIFdhcmQsIFN0cmVldCwgUXXhuq1uIDEsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2sus!4v1703076586166!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
